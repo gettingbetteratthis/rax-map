b033d7fa951a7dd241507a5c51843949d9421758
Tarmo Petrovits <tarmo@arego.ee>
2020-07-21 11:13:42 +0300

    Add pictures

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import { geolocated } from 'react-geolocated';
 import pic1 from './1.jpg';
 import pic2 from './2.jpg';
 import pic3 from './3.jpg';
@@ -8,15 +9,16 @@ import pic5 from './5.jpg';
 import pic6 from './6.jpg';
 import pic7 from './7.jpg';
 import pic8 from './8.jpg';
+import './index.css';
 
 const mapContainerStyle = {
-	width: '100vw',
-	height: '100vh',
+	width: '100%',
+	height: '100%',
 };
 
-const center = {
-	lat: 59.334394,
-	lng: 24.693515,
+const options = {
+	disableDefaultUI: true,
+	zoomControl: true,
 };
 
 const ll1 = {
@@ -58,15 +60,28 @@ const ll8 = {
 	lng: 24.692552,
 };
 
-function App() {
-	const [img1, setImg1] = useState(true);
-	const [img2, setImg2] = useState(true);
-	const [img3, setImg3] = useState(true);
-	const [img4, setImg4] = useState(true);
-	const [img5, setImg5] = useState(true);
-	const [img6, setImg6] = useState(true);
-	const [img7, setImg7] = useState(true);
-	const [img8, setImg8] = useState(true);
+function App(props) {
+	const [img1, setImg1] = useState(false);
+	const [img2, setImg2] = useState(false);
+	const [img3, setImg3] = useState(false);
+	const [img4, setImg4] = useState(false);
+	const [img5, setImg5] = useState(false);
+	const [img6, setImg6] = useState(false);
+	const [img7, setImg7] = useState(false);
+	const [img8, setImg8] = useState(false);
+
+	const handleCloseOverlay = (ev) => {
+		ev.preventDefault();
+		setImg1(false);
+		setImg2(false);
+		setImg3(false);
+		setImg4(false);
+		setImg5(false);
+		setImg6(false);
+		setImg7(false);
+		setImg8(false);
+	};
+
 	const { isLoaded, loadError } = useLoadScript({
 		googleMapsApiKey: 'AIzaSyBuqJwALXawnLryoAwguukKYN4GmWQmGCA',
 	});
@@ -78,18 +93,104 @@ function App() {
 			<GoogleMap
 				mapContainerStyle={mapContainerStyle}
 				zoom={17}
-				center={center}>
-				<Marker position={ll1} visible={img1} />
-				<Marker position={ll2} visible={img2} />
-				<Marker position={ll3} visible={img3} />
-				<Marker position={ll4} visible={img4} />
-				<Marker position={ll5} visible={img5} />
-				<Marker position={ll6} visible={img6} />
-				<Marker position={ll7} visible={img7} />
-				<Marker position={ll8} visible={img8} />
+				center={{
+					// lat: props?.coords?.latitude || 59.334394,
+					// lng: props?.coords?.longitude || 24.693515,
+					lat: 59.335221,
+					lng: 24.696972,
+				}}
+				options={options}>
+				<Marker position={ll1} onClick={() => setImg1(true)} />
+				<Marker position={ll2} onClick={() => setImg2(true)} />
+				<Marker position={ll3} onClick={() => setImg3(true)} />
+				<Marker position={ll4} onClick={() => setImg4(true)} />
+				<Marker position={ll5} onClick={() => setImg5(true)} />
+				<Marker position={ll6} onClick={() => setImg6(true)} />
+				<Marker position={ll7} onClick={() => setImg7(true)} />
+				<Marker position={ll8} onClick={() => setImg8(true)} />
+				{img1 && (
+					<div className='img-container'>
+						<button className='close' onClick={handleCloseOverlay}>
+							X
+						</button>
+
+						<img src={pic1} alt='pic1' />
+					</div>
+				)}
+				{img2 && (
+					<div className='img-container'>
+						<button className='close' onClick={handleCloseOverlay}>
+							X
+						</button>
+
+						<img src={pic2} alt='pic1' />
+					</div>
+				)}
+				{img3 && (
+					<div className='img-container'>
+						<button className='close' onClick={handleCloseOverlay}>
+							X
+						</button>
+
+						<img src={pic3} alt='pic1' />
+					</div>
+				)}
+				{img4 && (
+					<div className='img-container'>
+						<button className='close' onClick={handleCloseOverlay}>
+							X
+						</button>
+
+						<img src={pic4} alt='pic1' />
+					</div>
+				)}
+				{img5 && (
+					<div className='img-container'>
+						<button className='close' onClick={handleCloseOverlay}>
+							X
+						</button>
+
+						<img src={pic5} alt='pic1' />
+					</div>
+				)}
+				{img6 && (
+					<div className='img-container'>
+						<button className='close' onClick={handleCloseOverlay}>
+							X
+						</button>
+
+						<img src={pic6} alt='pic1' />
+					</div>
+				)}
+				{img7 && (
+					<div className='img-container'>
+						<button className='close' onClick={handleCloseOverlay}>
+							X
+						</button>
+
+						<img src={pic7} alt='pic1' />
+					</div>
+				)}
+				{img8 && (
+					<div className='img-container'>
+						<button className='close' onClick={handleCloseOverlay}>
+							X
+						</button>
+
+						<img src={pic8} alt='pic1' />
+					</div>
+				)}
 			</GoogleMap>
 		</div>
 	);
 }
 
-export default App;
+export default geolocated({
+	positionOptions: {
+		enableHighAccuracy: true,
+		maximumAge: 0,
+		timeout: Infinity,
+	},
+	watchPosition: true,
+	userDecisionTimeout: 5000,
+})(App);
